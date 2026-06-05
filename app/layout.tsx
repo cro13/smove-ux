ed2914f0b57618f2b806fbf38c8759647a795240
Emanuel Scirlet <cro13@pm.me>
2026-06-05 21:54:06 +0200

    fix: revert dynamic import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,8 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
-import dynamic from 'next/dynamic'
 
+import { ConvexClientProvider } from './ConvexClientProvider'
 import './globals.css'
-
-const ConvexClientProvider = dynamic(
-	() => import('./ConvexClientProvider').then((m) => m.ConvexClientProvider),
-	{ ssr: false }
-)
 
 const geistSans = Geist({
 	variable: '--font-sans',
